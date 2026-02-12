0fcd3afa7cf6acd01a284dd8d10ce2d4d387312e
Rehan Parveez <rehanparveez@gmail.com>
2026-02-12 15:12:27 +0500

    this commit is about the work i done today while continuing the learning of django celery project. firstly today removed the old database and migrations and then created new migrations for a new setup. after that created new periodic tasks of all types directly from the admin panel and then tested them properly like the solar event and crontab, periodic tasks for example. then also worked on the problem of the smtp email backend setup like yesterday on running the tasks from the server, celery worker and celery beat, the email sending part was not working correctly, but now its working successfully like today i received the email on my original account ruralrehan@gmail.com after running the task email send task. further today tried to clearly understand the difference b/w creating periodic tasks from the admin panel and creating them by a program using the intervalschedule and periodictask in code, and understood that mainly using only one approach at a time is better to avoid issues, and then also studied more about the celery configuration, scheduling behavior and like how internally the management of the tasks in django-celery-beat occurs.

--- learning/tasks.py
+++ learning/tasks.py
@@ -8,16 +8,14 @@ from django.core.mail import send_mail
 from django.contrib.auth.models import User
 
 @shared_task
-def calculate_task(a, b):
-    # this is a that multiplies by 2, adds b, then shows a delay and returns result.
-    sleep(5) 
-    calculation = (a * 2) + b
-    print(f"processing {a} and {b}, result: {calculation}")
-    return calculation
+def calculate_task(a=5, b=10):
+    sleep(5)
+    result = (a * 2) + b
+    print(f"processing {a} and {b}, result: {result}")
+    return result
 
 @shared_task
-def factorial_task(n):
-    # calculating a factorial of n with a small delay
+def factorial_task(n=5):
     sleep(7)
     result = 1
     for i in range(1, n + 1):
@@ -40,11 +38,13 @@ def clear_rabbitmq_data(key):
     print(f'RabbitMQ data cleared: {key}')
     return key
 
-# # creating schedule every 25 seconds
-# schedule, created = IntervalSchedule.objects.get_or_create(every=25, period=IntervalSchedule.SECONDS)
+# creating schedule every 25 seconds
+schedule, created = IntervalSchedule.objects.get_or_create(every=25, period=IntervalSchedule.SECONDS)
 
 # # scheduling the periodic task by program
-# PeriodicTask.objects.get_or_create(name='Clear RabbitMQ Periodic Task', task='learning.tasks.clear_rabbitmq_data', interval=schedule, args=json.dumps(['hello'])), # passing the arguments to the task as a JSON-encoded list
+# PeriodicTask.objects.get_or_create(name='factorial-task-every-10-seconds', task='learning.tasks.factorial_task', interval=schedule, args=json.dumps([5]))
+# PeriodicTask.objects.get_or_create(name='calculate-task-every-10-seconds', task='learning.tasks.calculate_task', interval=schedule, args=json.dumps([5, 10]))
+PeriodicTask.objects.get_or_create(name='Clear RabbitMQ Periodic Task', task='learning.tasks.clear_rabbitmq_data', interval=schedule, args=json.dumps(['hello'])), # passing the arguments to the task as a JSON-encoded list
 
 
 # email sending task with the console emailbackend
@@ -69,10 +69,10 @@ def send_email_smtp(subject="hello", message='this is a test email with the SMTP
     recipient_list = list(User.objects.values_list('email', flat=True))
 
     if recipient_list:
-        send_mail(subject=subject, message=message, from_email='rehanrural@gmail.com', recipient_list=recipient_list,
-            fail_silently=False)
-        print(f'email is sent {recipient_list}')
-        return f'email sent to {recipient_list}'
+        send_mail(subject=subject, message=message,
+            from_email='rehanrural@gmail.com', recipient_list=recipient_list, fail_silently=False)
+        print(f'the email is sent {recipient_list}')
+        return f'the email sent to {recipient_list}'
     else:
         print('no users found')
         return 'no recipients'
